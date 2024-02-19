4f84c1af2d91b1963c31147961534b0159ac4bc9
is52hertz <is.52hertz@gmail.com>
2024-02-20 07:10:01 +0800

    bash : git back.

--- bash/Installation.bash
+++ bash/Installation.bash
@@ -1,90 +1,90 @@
 # INSTALLATION BGING _____________________________________________________________
 # --- Language Pack ---
 # Chinese (Traditional)
-php ../composer.phar require flarum-lang/chinese-traditional
+composer require flarum-lang/chinese-traditional
 # Chinese (Simplified)
-php ../composer.phar require flarum-lang/chinese-simplified:dev-master
+composer require flarum-lang/chinese-simplified:dev-master
 # --- BBCode ---
-php ../composer.phar require ramesh-dada/bbcode-alerts #LINK:https://extiverse.com/extension/ramesh-dada/bbcode-alerts
-php ../composer.phar require nearata/flarum-ext-embed-video:"*" # 允许嵌入视频。
-php ../composer.phar require ramesh-dada/download-button #LINK：https://extiverse.com/extension/ramesh-dada/download-button
-php ../composer.phar require tohsakarat/table-of-content # 自动生成目录
-php ../composer.phar require datlechin/flarum-bbcode-hide-content:"*" # 隐藏内容? LINK:https://extiverse.com/extension/datlechin/flarum-bbcode-hide-content
-php ../composer.phar require litalino/flarum-more-bbcode:"*" # 回复/登录以查看隐藏内容... LINK:https://extiverse.com/extension/litalino/flarum-more-bbcode
-php ../composer.phar require wolfgang-demeter/flarum-ext-bbcode-5star-rating # 五星评价。LINK:https://extiverse.com/extension/wolfgang-demeter/flarum-ext-bbcode-5star-rating
-php ../composer.phar require flarumite/simple-spoilers:"*" # 点击查看。LINK:https://extiverse.com/extension/flarumite/simple-spoilers
-php ../composer.phar require 2nrwls/audio:"*" # 音频。LINK:https://extiverse.com/extension/2nrwls/audio
+composer require ramesh-dada/bbcode-alerts #LINK:https://extiverse.com/extension/ramesh-dada/bbcode-alerts
+composer require nearata/flarum-ext-embed-video:"*" # 允许嵌入视频。
+composer require ramesh-dada/download-button #LINK：https://extiverse.com/extension/ramesh-dada/download-button
+composer require tohsakarat/table-of-content # 自动生成目录
+composer require datlechin/flarum-bbcode-hide-content:"*" # 隐藏内容? LINK:https://extiverse.com/extension/datlechin/flarum-bbcode-hide-content
+composer require litalino/flarum-more-bbcode:"*" # 回复/登录以查看隐藏内容... LINK:https://extiverse.com/extension/litalino/flarum-more-bbcode
+composer require wolfgang-demeter/flarum-ext-bbcode-5star-rating # 五星评价。LINK:https://extiverse.com/extension/wolfgang-demeter/flarum-ext-bbcode-5star-rating
+composer require flarumite/simple-spoilers:"*" # 点击查看。LINK:https://extiverse.com/extension/flarumite/simple-spoilers
+composer require 2nrwls/audio:"*" # 音频。LINK:https://extiverse.com/extension/2nrwls/audio
 # --- Widgets ---
-php ../composer.phar require afrux/forum-widgets-core:"*"
-php ../composer.phar require afrux/online-users-widget:"*"
-php ../composer.phar require afrux/news-widget:"*"
-php ../composer.phar require afrux/forum-stats-widget:"*"
-php ../composer.phar require afrux/top-posters-widget:"*"
-php ../composer.phar require justoverclock/custom-html-widget:"*"
-php ../composer.phar require justoverclock/events-countdown:"*"
-php ../composer.phar require justoverclock/geo-weather:"*"
-php ../composer.phar require justoverclock/simple-calendar-widget:"*"
-php ../composer.phar require nearata/flarum-ext-discord-widget:"*"
-php ../composer.phar require datlechin/flarum-birthdays-widget:"*"
+composer require afrux/forum-widgets-core:"*"
+composer require afrux/online-users-widget:"*"
+composer require afrux/news-widget:"*"
+composer require afrux/forum-stats-widget:"*"
+composer require afrux/top-posters-widget:"*"
+composer require justoverclock/custom-html-widget:"*"
+composer require justoverclock/events-countdown:"*"
+composer require justoverclock/geo-weather:"*"
+composer require justoverclock/simple-calendar-widget:"*"
+composer require nearata/flarum-ext-discord-widget:"*"
+composer require datlechin/flarum-birthdays-widget:"*"
 # --- FOF ---
-php ../composer.phar require fof/upload "*"
-php ../composer.phar require fof/formatting:"*"
-php ../composer.phar require fof/links
-php ../composer.phar require fof/user-bio:"*"
-php ../composer.phar require fof/polls:"*"
-php ../composer.phar require fof/pages
-php ../composer.phar require fof/best-answer:"*"
-php ../composer.phar require fof/user-directory:"*"
-php ../composer.phar require fof/follow-tags:"*"
-php ../composer.phar require fof/byobu:"*"
-php ../composer.phar require fof/nightmode:"*"
-php ../composer.phar require fof/profile-image-crop:"*"
-php ../composer.phar require fof/reactions:"*"
-php ../composer.phar require fof/merge-discussions:"*"
-php ../composer.phar require fof/split:"*"
-php ../composer.phar require fof/drafts:"*"
-php ../composer.phar require fof/sitemap # !:QUESTION[https://extiverse.com/extension/fof/sitemap]!
-php ../composer.phar require fof/gamification
-php ../composer.phar require fof/analytics:*
-php ../composer.phar require fof/oauth
-php ../composer.phar require fof/default-user-preferences:"*"
-php ../composer.phar require fof/linguist
-php ../composer.phar require fof/moderator-notes:"*"
-php ../composer.phar require fof/impersonate:"*"
-php ../composer.phar require fof/pretty-mail:"*"
-php ../composer.phar require fof/terms
-php ../composer.phar require fof/socialprofile
-php ../composer.phar require fof/recaptcha:"*"
-php ../composer.phar require fof/ignore-users:"*"
-# php ../composer.phar require fof/forum-statistics-widget:"*"
-php ../composer.phar require fof/ban-ips:"*"
-php ../composer.phar require fof/subscribed:"*"
-php ../composer.phar require fof/default-group:"*"
-php ../composer.phar require fof/share-social
-php ../composer.phar require fof/username-request:"*"
-php ../composer.phar require fof/doorman:"*"
-php ../composer.phar require fof/prevent-necrobumping
-php ../composer.phar require fof/frontpage
-php ../composer.phar require fof/filter:"*"
-php ../composer.phar require fof/custom-footer:"*"
-php ../composer.phar require fof/webhooks
-php ../composer.phar require fof/sentry:"*"
-php ../composer.phar require fof/cookie-consent
-php ../composer.phar require fof/html-errors
-php ../composer.phar require fof/geoip:"*"
-php ../composer.phar require fof/discussion-language:"*"
-php ../composer.phar require fof/bbcode-details
+composer require fof/upload "*"
+composer require fof/formatting:"*"
+composer require fof/links
+composer require fof/user-bio:"*"
+composer require fof/polls:"*"
+composer require fof/pages
+composer require fof/best-answer:"*"
+composer require fof/user-directory:"*"
+composer require fof/follow-tags:"*"
+composer require fof/byobu:"*"
+composer require fof/nightmode:"*"
+composer require fof/profile-image-crop:"*"
+composer require fof/reactions:"*"
+composer require fof/merge-discussions:"*"
+composer require fof/split:"*"
+composer require fof/drafts:"*"
+composer require fof/sitemap # !:QUESTION[https://extiverse.com/extension/fof/sitemap]!
+composer require fof/gamification
+composer require fof/analytics:*
+composer require fof/oauth
+composer require fof/default-user-preferences:"*"
+composer require fof/linguist
+composer require fof/moderator-notes:"*"
+composer require fof/impersonate:"*"
+composer require fof/pretty-mail:"*"
+composer require fof/terms
+composer require fof/socialprofile
+composer require fof/recaptcha:"*"
+composer require fof/ignore-users:"*"
+# composer require fof/forum-statistics-widget:"*"
+composer require fof/ban-ips:"*"
+composer require fof/subscribed:"*"
+composer require fof/default-group:"*"
+composer require fof/share-social
+composer require fof/username-request:"*"
+composer require fof/doorman:"*"
+composer require fof/prevent-necrobumping
+composer require fof/frontpage
+composer require fof/filter:"*"
+composer require fof/custom-footer:"*"
+composer require fof/webhooks
+composer require fof/sentry:"*"
+composer require fof/cookie-consent
+composer require fof/html-errors
+composer require fof/geoip:"*"
+composer require fof/discussion-language:"*"
+composer require fof/bbcode-details
 # --- XLIVES ---
-php ../composer.phar require malago/flarum-achievements
-php ../composer.phar require the-turk/flarum-diff
-php ../composer.phar require clarkwinkelmann/flarum-ext-discussion-bookmarks
-php ../composer.phar require blomstra/fontawesome:"*"
+composer require malago/flarum-achievements
+composer require the-turk/flarum-diff
+composer require clarkwinkelmann/flarum-ext-discussion-bookmarks
+composer require blomstra/fontawesome:"*"
 php flarum assets:publish
 # --- Blog ---
-php ../composer.phar require v17development/flarum-blog
-php ../composer.phar require askvortsov/flarum-article-series:"*"
-php ../composer.phar require clamours/sticky-sidenav-blog
-php ../composer.phar require davwheat/blog-featured-widget:"*"
+composer require v17development/flarum-blog
+composer require askvortsov/flarum-article-series:"*"
+composer require clamours/sticky-sidenav-blog
+composer require davwheat/blog-featured-widget:"*"
 
 # END
 php flarum cache:clear
